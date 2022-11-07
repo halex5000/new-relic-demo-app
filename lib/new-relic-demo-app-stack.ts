cfd8dfca0af8c058d050901a4abe6574c1281b03
Alexander Hardman <alexander.hardman@gmail.com>
2022-11-07 11:05:03 -0500

    adding layer in

--- lib/new-relic-demo-app-stack.ts
+++ lib/new-relic-demo-app-stack.ts
@@ -1,6 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { LayerVersion, Runtime } from 'aws-cdk-lib/aws-lambda';
+import {config} from 'dotenv'
+
+config();
+
 
 export class NewRelicDemoAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -8,9 +13,15 @@ export class NewRelicDemoAppStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'NewRelicDemoAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const worker = new NodejsFunction(this, 'new-relic-observed-handler', {
+      description: 'a worker function to report metrics to New Relic',
+      environment: {
+        LAUNCHDARKLY_SDK_KEY: process.env.LAUNCHDARKLY_SDK_KEY || '',  
+      },
+      runtime: Runtime.NODEJS_16_X,
+      layers: [
+        LayerVersion.fromLayerVersionArn(this, 'new-relic-layer', 'arn:aws:lambda:us-west-2:451483290750:layer:NewRelicNodeJS16X:21')
+      ]
+    });
   }
 }
